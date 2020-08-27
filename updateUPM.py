adb5f7c0d4e5942df243001d18d86286e0487dd7
Rootjhon <354888562@qq.com>
2020-08-27 13:22:25 +0800

    1.8.8

--- updateUPM.py
+++ updateUPM.py
@@ -11,30 +11,45 @@
 # here put the import lib
 import os
 import shutil
+import json
 
-def updateUPM(packageName: str, tag: str):
+
+def updateUPM(packageName: str, version_tag: str):
     os.system("git checkout -f master")
+    modify_packageJson("Assets/{0}/package.json".format(packageName), version_tag)
     shutil.move("Assets/{0}".format(packageName), ".git/")
-    
+
     os.system("git checkout -f upm")
     os.system("git reset --hard")
     os.system("git clean -fd")
     os.system("git rm -rf --ignore-unmatch *")
-    
+
     dir_name = ".git/{0}/".format(packageName)
     for d in os.listdir(dir_name):
         shutil.move(dir_name + d, "./")
     shutil.rmtree(dir_name)
 
     os.system("git add -A")
-    os.system("git commit -m 'update upm to {0}'".format(tag))
-    os.system("git tag {0}".format(tag))
+    os.system("git commit -m 'update upm to {0}'".format(version_tag))
+    os.system("git tag {0}".format(version_tag))
     os.system("git push origin upm --tags")
 
     os.system("git checkout -f master")
 
+
+def modify_packageJson(package_path: str, version_tag: str):
+    json_item = ""
+    with open(package_path, 'r') as fileItem:
+        json_item = json.load(fileItem)
+        json_item["version"] = version_tag
+    with open(package_path, 'wb') as fileItem:
+        fileItem.write(json.dumps(json_item, separators=(",", ":"), indent=4).encode())
+        fileItem.flush()
+    pass
+
+
 if __name__ == "__main__":
-    tag = "1.8.6"
+    tag = "1.8.8"
     packageName = "AssetBundles-Browser"
     # updateUPM(packageName,tag)
     pass
